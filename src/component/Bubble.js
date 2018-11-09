882baaebecba3c9e74e90ecbfd1586f92ec7bec4
kaizengjun <kaizengjun@163.com>
2018-11-09 15:55:25 +0800

    Add Bubble sort

---
+++ src/component/Bubble.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Bubble extends Component {
+  render () {
+    return (
+      <div>
+        Bubble
+      </div>
+    )
+  }
+}
+
+export default Bubble
